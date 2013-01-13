484c82d1af74b86da410a50d7600605080c49fbd
Yuriy Vasiyarov <varyous@gmail.com>
2013-01-13 18:00:40 +0400

    Boolean decoder is ok

--- encoder.go
+++ encoder.go
@@ -1,10 +1,12 @@
 package php_session_decoder;
-/*
+
 import (
     "errors"
+    "bytes"
+    "strings"
     "fmt"
 )
-*/
+const VALUES_SEPARATOR = '|'
 
 /*
 func encode() {
@@ -17,7 +19,7 @@ type PhpValue interface{}
 type PhpSessionData map[string]PhpValue
 
 type PhpDecoder struct {
-    source string
+    source *strings.Reader
     position int
     data *PhpSessionData
 }
@@ -25,13 +27,79 @@ type PhpDecoder struct {
 func NewPhpDecoder(phpSession string) (*PhpDecoder) {
     sessionData := make(PhpSessionData)
     d := &PhpDecoder{
-        source: phpSession, 
+        source: strings.NewReader(phpSession), 
         position: 0,
         data: &sessionData,
     }
     return d
 }
 
+/*
 func (decoder *PhpDecoder)Decode() (*PhpSessionData, error) {
-    return decoder.data, nil
+    var (
+        token byte
+        err error
+    )
+    
+    if valueName, err := decoder.readUntil(VALUES_SEPARATOR, false); err == nil {
+        if value, err:= decoder.DecodeValue(); err == nil {
+            decoder.data[string(valueName)] = value
+        } else {
+            errors.New("Can not read variable name:" + string(decoder.readUntil(VALUES_SEPARATOR, true)))
+        }
+    } else {
+       err = errors.New("Can not read variable name")
+    }
+    return decoder.data, err
 }
+*/
+
+func (decoder *PhpDecoder)DecodeValue() (PhpValue, error) {
+    var (
+        value PhpValue
+        err error
+    )
+    
+    if token, err := decoder.source.ReadByte(); err == nil {
+        decoder.expect(':')
+        switch token {
+            case 'b': 
+                if rawValue, _, _err := decoder.source.ReadRune(); _err == nil {
+                    value = rawValue == '1'
+                    err = errors.New("Can not read boolean value:")
+                } else {
+                    err = errors.New("Can not read boolean value")
+                }
+ 
+                decoder.expect(';')
+        }
+    }
+    return value, err
+}
+
+func (decoder *PhpDecoder) readUntil(stopByte byte) ([]byte, error) {
+    result := new(bytes.Buffer)
+    var (
+        token byte
+        err error
+    )
+    for {
+        if token, err = decoder.source.ReadByte(); err != nil || token == stopByte {
+            break;
+        } else {
+            result.WriteByte(token)
+        }
+    }
+    return result.Bytes(), err
+}
+
+func (decoder *PhpDecoder) expect(expectRune rune) (error) {
+    token, _, err := decoder.source.ReadRune(); 
+    if err != nil {
+        err = errors.New(fmt.Sprintf("Can not read expected: %v", expectRune))
+    } else if token == expectRune {
+        err = errors.New(fmt.Sprintf("Read %v, but expected: %v", token, expectRune))
+    }
+    return err
+}
+
--- encoder_test.go
+++ encoder_test.go
@@ -4,8 +4,10 @@ import (
     "testing"
 )
 
-//"login_ok|b:1;"
-//"login_ok|b:1;inteiro|i:34;"
+const BOOLEAN_VALUE_ENCODED = "login_ok|b:1;"
+const BOOLEAN_VALUE_ENCODED_WITHOUT_NAME = "b:1;"
+const BOOLEAN_AND_INT_ENCODED = "login_ok|b:1;inteiro|i:34;"
+
 func TestDecoderFabrica(t *testing.T) {
     decoder := NewPhpDecoder("")
     if decoder == nil {
@@ -13,14 +15,45 @@ func TestDecoderFabrica(t *testing.T) {
     }
 }
 
+func TestDecodeBooleanValueWithoutName(t *testing.T) {
+    decoder := NewPhpDecoder(BOOLEAN_VALUE_ENCODED_WITHOUT_NAME)
+    if result, err := decoder.DecodeValue(); err != nil {
+        t.Errorf("Can not decode boolens value %#v \n", err)
+    } else {
+        if v, ok := (result).(bool); !ok {
+            t.Errorf("Boolean value was not decoded \n")
+        } else if v != true {
+            t.Errorf("Boolean value was incorrectly decoded \n")
+        }
+    }
+}
+/*
 func TestDecodeBooleanValue(t *testing.T) {
-    decoder := NewPhpDecoder("login_ok|b:1;")
+    decoder := NewPhpDecoder(BOOLEAN_VALUE_ENCODED)
+    if result, err := decoder.Decode(); err != nil {
+        t.Errorf("Can not decode boolens value %#v \n", err)
+    } else {
+        if v, ok := (*result)["login_ok"]; !ok {
+            t.Errorf("Boolean value was not decoded \n")
+        } else if v != true {
+            t.Errorf("Boolean value was incorrectly decoded \n")
+        }
+    }
+}
+
+func TestDecodeIntValue(t *testing.T) {
+    decoder := NewPhpDecoder(BOOLEAN_AND_INT_ENCODED)
     if result, err := decoder.Decode(); err != nil {
         t.Errorf("Can not decode boolens value %#v \n", err)
     } else {
         if _, ok := (*result)["login_ok"]; ok {
             t.Errorf("Boolean value was not decoded \n")
         }
+        if _, ok := (*result)["inteiro"]; ok {
+            t.Errorf("Int value was not decoded \n")
+        }
     }
 }
+*/
+
 
