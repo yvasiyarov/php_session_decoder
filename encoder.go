78d72bdc46e4e98f8c7e3066731b741a2ca58771
Yuriy Vasiyarov <varyous@gmail.com>
2013-03-13 10:10:43 +0400

    Boolean values encoded

--- encoder.go
+++ encoder.go
@@ -3,7 +3,7 @@ package php_session_decoder
 import (
 	"bytes"
 //	"errors"
-//	"fmt"
+	"fmt"
 //	"strconv"
 //	"strings"
 )
@@ -11,10 +11,10 @@ import (
 
 type PhpEncoder struct {
 	dest     bytes.Buffer
-	data     *PhpSessionData
+	data     PhpSessionData
 }
 
-func NewPhpEncoder(sessionData *PhpSessionData) *PhpEncoder {
+func NewPhpEncoder(sessionData PhpSessionData) *PhpEncoder {
         var buffer bytes.Buffer
 	e := &PhpEncoder{
 		dest:   buffer,
@@ -23,3 +23,45 @@ func NewPhpEncoder(sessionData *PhpSessionData) *PhpEncoder {
 	return e
 }
 
+//todo: check if root key will be some not ansi string
+func (encoder *PhpEncoder) Encode() (string, error) {
+	var resultErr error
+        
+        if encoder.data == nil {
+            return "", nil
+        } 
+
+	for k, v := range encoder.data {
+		encoder.dest.WriteString(k)
+		encoder.dest.WriteRune(VALUE_NAME_SEPARATOR)
+		if resultErr = encoder.encodeValue(v); resultErr != nil {
+		    break
+		}
+		encoder.dest.WriteRune(VALUES_SEPARATOR)
+	}
+	return encoder.dest.String(), resultErr
+}
+
+func (encoder *PhpEncoder) encodeValue(value PhpValue) (error) {
+    var err error
+    switch t := value.(type) {
+    default:
+            err = fmt.Errorf("Unexpected type %T", t)
+    case bool:
+            encoder.dest.WriteString("b")
+            encoder.dest.WriteRune(TYPE_VALUE_SEPARATOR)
+            if bValue, _ := value.(bool); bValue {
+                encoder.dest.WriteString("1")
+            } else {
+                encoder.dest.WriteString("0")
+            }
+    //case string:
+//        stringKey, _ := k.(string)
+//    #        value[stringKey] = v
+//    #case int:
+//    #        intKey, _ := k.(int)
+//    #        strKey := strconv.Itoa(intKey)
+//    #        value[strKey] = v
+    }
+    return err 
+}
--- encoder_test.go
+++ encoder_test.go
@@ -6,21 +6,21 @@ import (
 
 func TestEncoderFabrica(t *testing.T) {
         var data PhpSessionData
-	encoder := NewPhpEncoder(&data)
+	encoder := NewPhpEncoder(data)
 	if encoder == nil {
 		t.Error("Can not create encoder object\n")
 	}
 }
 
 func TestEncodeBooleanValue(t *testing.T) {
-        var data PhpSessionData
+        data := make(PhpSessionData)
         data["login_ok"] = true 
-	encoder := NewPhpDecoder(data)
+	encoder := NewPhpEncoder(data)
 	if result, err := encoder.Encode(); err != nil {
 		t.Errorf("Can not encode boolens value %#v \n", err)
 	} else {
 		if result != BOOLEAN_VALUE_ENCODED {
-			t.Errorf("Boolean value was encoded incorrectly \n")
+			t.Errorf("Boolean value was encoded incorrectly %v \n", result)
 		}
 	}
 }
